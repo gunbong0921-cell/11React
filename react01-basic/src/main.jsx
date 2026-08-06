96e2b300d84fd32e533ba1b5495660aa72ad3dc8
gunbong0921 <gunbong0921@gmail.com>
2026-08-06 16:14:29 +0900

    3-2

--- react01-basic/src/main.jsx
+++ react01-basic/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App-09ShallowComp.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <App />
