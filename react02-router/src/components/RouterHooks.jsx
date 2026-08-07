5231a894ddeabbe259f208785ec1e3ff9c97753a
gunbong0921 <gunbong0921@gmail.com>
2026-08-07 12:11:14 +0900

    4

--- react02-router/src/components/RouterHooks.jsx
+++ react02-router/src/components/RouterHooks.jsx
@@ -1,21 +1,37 @@
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 const RouterHooks = () => {
   const location = useLocation();
-  const navigate = useNavigate();
-  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const mode = searchParams.get('mode');
+  const pageNum = searchParams.get('pageNum');
 
+  const changeMode = () => {
+    const nextMode = mode === 'list' ? 'view' : 'list';
+    setSearchParams({ mode: nextMode, pageNum });
+  }
+
+  const nextPage = () => {
+    let pageTemp = (pageNum===null || isNaN(pageNum)) ? 1 : parseInt(pageNum) + 1;
+    setSearchParams({ mode, pageNum: pageTemp });
+  }
+
+  const prevPage = () => {
+    let pageTemp = (pageNum===null || isNaN(pageNum)) ? 1 : parseInt(pageNum) - 1;
+    setSearchParams({ mode, pageNum: pageTemp });
+  }
   return (
     <div>
       <h3>Router 관련 Hook</h3>
       <ul>
-        <li>pathname: {location.pathname}</li>
-        <li>search: {location.search || '(없음)'}</li>
-        <li>params: {JSON.stringify(params)}</li>
+        <li>URL : {location.pathname}</li>
+        <li>QueryString : {location.search}</li>
+        <li>Mode : {mode}</li>
+        <li>PageNum : {pageNum}</li>
       </ul>
-      <button type="button" onClick={() => navigate('/')}>
-        Home으로 이동
-      </button>
+      <button type="button" onClick={changeMode}>Mode 변경</button>
+      <button type="button" onClick={nextPage}>다음 Page</button>
+      <button type="button" onClick={prevPage}>이전 Page</button>
     </div>
   );
 }
--- react02-router/src/components/TopNavi.jsx
+++ react02-router/src/components/TopNavi.jsx
@@ -10,8 +10,11 @@ const TopNavi = () => {
       {/* a태그를 사용하는 경우에는 화면의 깜박임이 있으므로 합성이벤트 객체를 통해 반드시 
       preventDefault() 메서드를 호출해야한다. */}
       <a href="/">Home</a>&nbsp;
+      {/* NavLink 컴포넌트의 경우 링크를 클릭하면 엘리먼트에 active 클래스 속성을 자동으로 추가해준다.
+      이를 통해 CSS 스타일을 적용할 수 있다. */}
       <NavLink to="/intro">인트로</NavLink>&nbsp;
       <NavLink to="/intro/router">Router관련Hook</NavLink>&nbsp;
+      {/* Link 컴포넌트는 NavLink와 달리 active 클래스 속성을 자동으로 추가하지 않는다. */}
       <Link to="/xyz">잘못된URL</Link>&nbsp;
     </nav>
   );
