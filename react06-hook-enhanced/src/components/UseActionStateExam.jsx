9f9fd75462b22f1a313b103cf8e75a430592c703
gunbong0921 <gunbong0921@gmail.com>
2026-08-12 10:49:56 +0900

    1

--- react06-hook-enhanced/src/components/UseActionStateExam.jsx
+++ react06-hook-enhanced/src/components/UseActionStateExam.jsx
@@ -1,12 +1,17 @@
 import { useActionState } from 'react';
 
+//로그인 처리를 위한 비동기 함수 정의 
 async function authLogin(prevState, formData) {
+  //첫번째 매개변수인 이전상태를 콘솔로 확인 
   console.log('prevState', prevState);
+  //사용자가 입력한 폼값을 읽음
   const userid = formData.get('userid');
   const userpw = formData.get('userpw');
 
+  //1초 대기 후 로그인 처리 
   await new Promise(resolve => setTimeout(resolve, 1000));
 
+  //로그인 여부 판단 
   if (userid === 'kosmo' && userpw === '1234') {
     return "로그인 성공";
   }
@@ -16,6 +21,13 @@ async function authLogin(prevState, formData) {
 }
 
 const UseActionStateExam = () => {
+  /**
+  message : authLogin 함수의 반환값을 저장하는 상태 변수 
+  formAction : 폼 제출 시 실행할 액션 함수 
+  isPending : 비동기 작업 진행 중인지 여부를 나타내는 불리언 값
+  authLogin : 로그인 처리를 위한 비동기 함수
+  null : 초기값 즉, 초기상태를 나타내는 값
+   */
   const [message, formAction, isPending] = useActionState(authLogin, null);
 
   return (
@@ -25,6 +37,7 @@ const UseActionStateExam = () => {
       아이디 : <input type="text" name="userid" /> <br />
       비밀번호 : <input type="password" name="userpw" /> <br />
       <button type="submit">로그인</button>
+      {/* 로그인 처리 중 로딩 메세지 표현. 폼값이 제출중이라면 true를 반환하나, 그렇지 않다면 message 상태값을 표현. */}
       {isPending ? 'Loading...' : message}
     </form>
     </>
--- react06-hook-enhanced/src/components/UseFormStatusExam.jsx
+++ react06-hook-enhanced/src/components/UseFormStatusExam.jsx
@@ -1,6 +1,7 @@
 import { useFormStatus } from 'react-dom';
 import { useState } from 'react';
 
+//이름을 받은 후 1초후 완료메세지를 반환해주는 비동기 함수 정의 
 async function submitForm(formData) {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -9,12 +10,21 @@ async function submitForm(formData) {
   });
 }
 
+//제출 버튼 컴포넌트 정의 
 const SubmitButton = () => {
+  /**
+  pending : 비동기 작업 진행 중인지 여부를 나타내는 불리언 값
+  data : 비동기 작업의 결과를 저장하는 값
+  method : 폼 제출 방법(POST, PUT, DELETE 등)
+  action : 폼 제출 시 실행할 액션 함수
+   */
   const { pending, data, method, action } = useFormStatus();
   console.log('data', data);
   console.log('method', method);
   console.log('action', action);
   return (
+    //padding의 상태에 따라 버튼을 비활성화 하거나 텍스트를 변경
+    //특히 버튼이 비활성화되면 사용자가 버튼을 클릭할 수 없게 함
     <button type="submit" disabled={pending}>
       {pending ? '제출중...' : '제출'}
     </button>
@@ -22,9 +32,12 @@ const SubmitButton = () => {
 };
 
 const UseFormStatusExam = () => {
+  //메세지를 저장하는 상태 변수 
   const [message, setMessage] = useState('');
 
+  //폼 제출 시 실행할 액션 함수 정의 
   const handleSubmit = async (formData) => {
+    //전송된 폼데이터를 submitForm 함수에 전달하고 결과를 받음
     const result = await submitForm(formData);
     setMessage(result);
   };
@@ -34,6 +47,7 @@ const UseFormStatusExam = () => {
       <h2>useFormStatus 사용하기</h2>
       <form action={handleSubmit}>
         <label>
+          {/* required 속성은 사용자가 이름을 입력하지 않으면 폼을 제출할 수 없게 함 */}
           이름 : <input type="text" name="name" required />
         </label>
         <SubmitButton />
